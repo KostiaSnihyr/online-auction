7d4505228951a59d4dc7df0d7442af616eb96bb2
kosssm155 <kosssm155@gmail.com>
2020-02-04 18:08:03 +0200

    add link for prod

--- script/utilities.js
+++ script/utilities.js
@@ -17,16 +17,19 @@ function createProd(prod) {
     const img = document.createElement('img');
     const description = document.createElement('p');
     const title = document.createElement('h3');
+    const titleLink = document.createElement('a');
 
     prodWrapper.className = 'product';
     img.className = 'product-img';
+    titleLink.setAttribute('href', 'product.html?id=' + prod.id);
 
     img.setAttribute('src', prod.image);
     description.innerHTML = "price: ".concat(prod.price, " <br> color: ").concat(prod.color, " <br> rank: ").concat(prod.rank);
     if ('size' in prod) {
        description.innerHTML += " <br> size ".concat(prod.size);
     }
-    title.innerText = prod.title;
+    titleLink.innerText = prod.title;
+    title.appendChild(titleLink);
 
     prodWrapper.appendChild(img);
     prodWrapper.appendChild(title);
